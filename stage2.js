9751db4520404da0b452e35f952bee636884006a
Kira Fernandez <kf1280@messiah.edu>
2018-01-24 22:19:38 -0500

    Staged .js files.

---
+++ stage2.js
@@ -0,0 +1,75 @@
+let renderer,
+  scene,
+  camera,
+  myCanvas = document.getElementById('myCanvas');
+
+  // RENDERER
+  renderer = new THREE.WebGLRenderer({canvas: myCanvas, antialias: true});
+  renderer.setClearColor(0x000000);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // CAMERA
+  camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 10000);
+
+  // SCENE
+  scene = new THREE.Scene();
+
+  // LIGHTS
+  let light = new THREE.AmbientLight(0xffffff, 0.5);
+  scene.add(light);
+
+  let light2 = new THREE.PointLight(0xffffff, 0.5);
+
+  scene.add(light2);
+
+  let loader = new THREE.FontLoader();
+  let font = loader.parse(comicsansJSON);
+  let textString = "MEMES";
+  let geometry = new THREE.TextGeometry(textString, {font: font, size: 120, height: 10, bevelThickness: 1, extrudeMaterial: 1});
+  let material = new THREE.MeshLambertMaterial({color: 0xF3FFE2});
+
+  let mesh = new THREE.Mesh(geometry, material);
+  mesh.position.z = -100;
+
+  let pivot = new THREE.Group();
+  pivot.add(mesh);
+  scene.add(pivot);
+
+  camera.position.set(300, 30, 900);
+
+  let reached_far = false;
+  let reached_near = false;
+  let position = 10;
+
+  function render() {
+
+    camera.rotation.z += 0.01;
+    pivot.rotation.y = 19.5;
+
+    if (!reached_far && !reached_near) {
+      camera.position.z += 10;
+    } else if (reached_far && !reached_near) {
+      camera.position.z -= 10;
+    } else if (reached_far && reached_near) {
+      reached_far = false;
+      reached_near = false;
+      camera.position.z += 10;
+    }
+
+    if (camera.position.z == 3000) {
+      reached_far = true;
+    }
+
+    if (camera.position.z == 900) {
+      reached_near = true;
+    }
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+
+  }
+
+  //RENDER LOOP
+  render();
